6f7144141b45d5b40b171eaabda5c05b2c11394d
Hakan Durmaz <hakandurmz@hotmail.com>
2021-04-07 18:27:06 +0300

    finished auth0 and mailing service settings

---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
+
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/private", require("./routes/private"));
+
+//Error handler
+app.use(errorHandler);
+
+const server = app.listen(PORT, () => {
+  console.log("client on port 5000");
+});
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`logged error: ${err}`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
